0833bc97d843c53ab6b443209a417e0f7acac027
SRUJANA PARAPATAKAM <psrujanareddy20@gmail.com>
2021-07-02 14:59:16 +0530

    Add project files.

---
+++ OrderItem/Controllers/OrderController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using OrderItem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OrderItem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        // GET: api/<OrderController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<OrderController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<OrderController>
+        [HttpPost]
+        public Cart Post([FromBody] int menuitemid)
+        {
+            Cart c = new Cart();
+            c.Id = 1;
+            c.userId = 1;
+            c.menuItemId = menuitemid;
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+            using (HttpClient client = new HttpClient(clientHandler))
+            {
+                client.BaseAddress = new Uri("https://localhost:44382/api/");
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = new HttpResponseMessage();
+                response = client.GetAsync("MenuItemController_cs/" + menuitemid).Result;
+                string claimStatus = response.Content.ReadAsStringAsync().Result;
+                c.menuItemName = claimStatus;
+            }
+            return c;
+        }
+
+        // PUT api/<OrderController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<OrderController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
